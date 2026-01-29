06e2db0ed5bd761de28a75dfd6981d39c1bf5e81
Ratu8P <99942827+Ratu8P@users.noreply.github.com>
2026-01-29 08:59:28 +0000

    Deploying to gh-pages from @ Ratu8P/Ratu8P.github.io@b55cea7753da4de4fe4afc46ee0b024df4284fee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -295,7 +295,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/assets/pdf/example_pdf.pdf";
+            window.location.href = "/assets/pdf/Resume_Haobo_Zhang.pdf";
           
         },
       },{id: "post-a-post-with-diagrams",
